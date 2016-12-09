ca95b6a9d8e78e88ba3ce563475b0abc6dd6e0d8
xiaoxiaohong666 <18330689533@163.com>
2016-12-09 17:49:12 +0800

    js

--- js/index.js
+++ js/index.js
@@ -28,40 +28,40 @@ $(document).ready(function(){
 //        将这张图片放在其他后面，将ul的宽设置成图片个数*图片宽度
         $(".ImgOut").append(firstimg).width($(".ImgOut li").length*($(".ImgOut img").width()));
         //  点击下一个rightbutton
-        $(".rightButton").click(function(){
-            i++;
-            if(i==$(".ImgOut li").length){
-                i=1;
-                $(".ImgOut").css("left",0);
-            }
-
-            $(".ImgOut").stop().animate({left:-i*600},1000);
-//            添加小圆点的样式
-            if(i==$(".ImgOut li").length-1){
-                $(".circle li").eq(0).addClass("active").siblings().removeClass("active");
-            }else{
-                $(".circle li").eq(i).addClass("active").siblings().removeClass("active");
-            }
-        });
-//        点击上一个leftButton
-        $(".leftButton").click(function(){
-            i--;
-            if(i==-1){
-                i=$('.ImgOut li').length-2;
-                $('.ImgOut').css({left:-($('.ImgOut li').length-1)*600});
-            }
-            $(".ImgOut").stop().animate({left:-i*600},1000);
-            $(".circle li").eq(i).addClass("active").siblings().removeClass("active");
-        });
-        $(".wrap").hover(function(){
-            $(".Button").show();
-        },function(){
-            $(".Button").hide();
-        })
+//        $(".rightButton").click(function(){
+//            i++;
+//            if(i==$(".ImgOut li").length){
+//                i=1;
+//                $(".ImgOut").css("left",0);
+//            }
+//
+//            $(".ImgOut").stop().animate({left:-i*600},1000);
+////            添加小圆点的样式
+//            if(i==$(".ImgOut li").length-1){
+//                $(".circle li").eq(0).addClass("active").siblings().removeClass("active");
+//            }else{
+//                $(".circle li").eq(i).addClass("active").siblings().removeClass("active");
+//            }
+//        });
+////        点击上一个leftButton
+//        $(".leftButton").click(function(){
+//            i--;
+//            if(i==-1){
+//                i=$('.ImgOut li').length-2;
+//                $('.ImgOut').css({left:-($('.ImgOut li').length-1)*600});
+//            }
+//            $(".ImgOut").stop().animate({left:-i*600},1000);
+//            $(".circle li").eq(i).addClass("active").siblings().removeClass("active");
+//        });
+//        $(".wrap").hover(function(){
+//            $(".Button").show();
+//        },function(){
+//            $(".Button").hide();
+//        })
 //        鼠标划入圆点
         $(".circle li").mouseover(function(){
             var _index = $(this).index();
-            $(".ImgOut").stop().animate({left:-_index*600},1000);
+            $(".ImgOut").stop().animate({left:-_index*600},2000);
             $(".circle li").eq(_index).addClass("active").siblings().removeClass("active");
         });
 
@@ -72,13 +72,13 @@ $(document).ready(function(){
                 i=1;
                 $('.ImgOut').css({left:0});
             }
-            $(".ImgOut").stop().animate({left:-i*600},1000);
+            $(".ImgOut").stop().animate({left:-i*600},2000);
             if(i==$(".ImgOut li").length-1){
                 $(".circle li").eq(0).addClass("active").siblings().removeClass("active");
             }else{
                 $(".circle li").eq(i).addClass("active").siblings().removeClass("active");
             }
-        },2000);
+        },3000);
         $(".wrap").hover(function(){
             clearInterval(timer);
         },function(){
@@ -88,13 +88,13 @@ $(document).ready(function(){
                     i=1;
                     $('.ImgOut').css({left:0});
                 }
-                $(".ImgOut").stop().animate({left:-i*600},1000);
+                $(".ImgOut").stop().animate({left:-i*600},2000);
                 if(i==$(".ImgOut li").length-1){
                     $(".circle li").eq(0).addClass("active").siblings().removeClass("active");
                 }else{
                     $(".circle li").eq(i).addClass("active").siblings().removeClass("active");
                 }
-            },2000);
+            },3000);
         })
     })
 });
